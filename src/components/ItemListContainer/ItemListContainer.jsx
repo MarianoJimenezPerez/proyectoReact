a5e50ecfaeef2df73a9e7c715836af2d4ee26abd
Mariano Jimenez Perez <marianojimenezperez1@gmail.com>
2021-08-07 14:28:14 -0300

    se trabaja con el nav

---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const ItemListContainer = ({nombre}) => {
+
+    return(
+
+        <div>
+            <p className="t-center m-10">{nombre}</p>
+        </div>        
+    ) 
+}
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const NavBar = () => {
+
+    return(
+
+        <header className="header t-center">
+            <nav>
+                <h2>Mi Logo</h2>
+                <ul className="ul-nav">
+                    <li>Inicio</li>
+                    <li>Tienda</li>
+                    <li>Quienes somos</li>
+                    <li>Contacto</li>
+                </ul>
+            </nav>            
+        </header>        
+    ) 
+}
